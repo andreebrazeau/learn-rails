c60e28b105b7204ea4613adf24d39b5a963a3ba1
Andrée Brazeau <abrazeau@bitcasa.com>
2016-01-02 11:45:57 -0800

    sending email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,11 +28,8 @@ Rails.application.configure do
   config.assets.debug = true
 
   config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: Rails.application.secrets.domain_name,
-      authentication: "plain",
-      enable_starttls_auto: true,
+      address: "smtp.mandrillapp.com",
+      port: 2525,
       user_name: Rails.application.secrets.email_provider_username,
       password: Rails.application.secrets.email_provider_password
   }
@@ -40,6 +37,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
