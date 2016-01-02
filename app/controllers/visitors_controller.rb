d87e68361ccc21c584d544ffd024344b59367d49
Andrée Brazeau <abrazeau@bitcasa.com>
2016-01-01 21:47:22 -0800

    front-end framework set

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,7 +4,5 @@ class VisitorsController < ApplicationController
     @owner = Owner.new # @ means this will be available in the view.
     # render 'visitors/new' it not needed because of magic
     # Rails will be looking at the view visitor/new !
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
   end
 end
