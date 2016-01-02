cd0a846feda3e61e32ffe5a82a1c7c571e7e892f
Andrée Brazeau <abrazeau@bitcasa.com>
2016-01-01 22:30:11 -0800

    contact form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,29 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(secure_params)
+    if @contact.valid?
+      #TODO send message
+      flash[:notce] = "Message sent from #{@contact.name}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+
+  # def process_form
+  #   message = "Received request from #{params[:contact][:name]}"
+  #   redirect_to root_path, :notice => message
+  # end
+  #
+end
